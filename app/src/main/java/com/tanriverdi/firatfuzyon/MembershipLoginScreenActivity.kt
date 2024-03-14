a551e4c51b3ac8b153338f610341e66da9eefad5
abdullah-tanriverdi <tanriverdi.firatuni@gmail.com>
2024-03-14 16:14:23 +0300

    Sign In Screen Coded

--- app/src/main/java/com/tanriverdi/firatfuzyon/MembershipLoginScreenActivity.kt
+++ app/src/main/java/com/tanriverdi/firatfuzyon/MembershipLoginScreenActivity.kt
@@ -21,21 +21,28 @@ class MembershipLoginScreenActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // Buton işlevlerini ayarla
         buttons()
 
+        // Firebase yetkilendirme örneğini al
        auth =Firebase.auth
 
     }
 
 
 
+    // Butonların tıklama işlevlerini tanımla
     fun buttons(){
+
+        // İletişim ekranını başlatan buton
         binding.membershipCardViewContactUsText.setOnClickListener{
             //  Intent ile ContactUsActivity'yi başlat
             val intent = Intent(this@MembershipLoginScreenActivity,ContactUsActivity::class.java)
             startActivity(intent)
         }
 
+
+        // Gizlilik politikası ekranını başlatan buton
         binding.membersipSecurityText.setOnClickListener{
             // Intent ile PrivacyPolicyActivity'yi başlat
             val intent = Intent(this@MembershipLoginScreenActivity,PrivacyPolicyActivity::class.java)
@@ -43,36 +50,42 @@ class MembershipLoginScreenActivity : AppCompatActivity() {
         }
 
 
+        // Üye olma ekranını başlatan buton
         binding.membershipSignUpText.setOnClickListener {
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
 
+        // Şifremi unuttum ekranını başlatan buton
         binding.membershipCardViewForgotPasswordText.setOnClickListener {
             val intent= Intent(this, ForgotPasswordActivity::class.java)
             startActivity(intent)
 
         }
 
+        // Giriş yapma butonu
         binding.membershipCardViewLoginTextButton.setOnClickListener {
             val email = binding.membershipCardViewUsernameEditText.text.toString()
           val password = binding.membershipCardViewPasswordEditText.text.toString()
 
-           if (email.equals("") || password.equals("")){
-               Toast.makeText(this, "basasrızı", Toast.LENGTH_SHORT).show()
-
-            }else{
-                auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
-                    val intent = Intent(this,UyelikHomeActivity::class.java)
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Lütfen e-posta ve şifrenizi girin", Toast.LENGTH_SHORT).show()
+            } else if (!email.endsWith("@firat.edu.tr")) {
+                Toast.makeText(this, "Sadece @firat.edu.tr uzantılı e-posta adresleri kabul edilmektedir", Toast.LENGTH_SHORT).show()
+            } else {
+                // Firebase üzerinden giriş yap
+                auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                    // Başarılı giriş
+                    Toast.makeText(this, "Giriş başarılı", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, UyelikHomeActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
                     finish()
                 }.addOnFailureListener {
-                    Toast.makeText(this,it.localizedMessage,Toast.LENGTH_SHORT).show()
-
+                    // Giriş başarısız
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
                 }
-           }
-
+            }
 
         }
     }
